b653c1ea9b0f2667d4a770b4fc690e85111bd4e9
roberts-sandbox <ras.slis@gmail.com>
2013-03-14 23:38:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,19 @@
-from rootpy.tree import Tree, TreeModel
-from rootpy.types import FloatCol, BoolCol
-from rootpy.io import root_open
-from random import gauss
-import random
+#!/usr/bin/env python
+import argparse
+import math
 
-random.seed(0)
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Compute tail p-value of the normal distribution')
+parser.add_argument('zvalue', type=float,
+                   help='z-value for which you want the corresponding p-value')
+parser.add_argument('-m', '--mean', type=float, default=0,
+                   help='Mean of the normal distribution')
+parser.add_argument('-s', '--sigma', type=float, default=1,
+                   help='Witdh (standard deviation) of the normal distribution')
+args = parser.parse_args()
 
-
-class Sample(TreeModel):
-    a = FloatCol()
-    b = FloatCol()
-    label = BoolCol()
-
-
-with root_open('sample.root', 'recreate'):
-    tree = Tree('sample', model=Sample)
-    for i in xrange(int(1e4)):
-        if i % 4 == 0:
-            tree.a = gauss(1, 1)
-            tree.b = gauss(1, 1)
-            tree.label = True
-        elif i % 4 == 1:
-            tree.a = gauss(1, 1)
-            tree.b = gauss(-1.5, 1)
-            tree.label = False
-        elif i % 4 == 2:
-            tree.a = gauss(-1.5, 1)
-            tree.b = gauss(-1, 1)
-            tree.label = True
-        else:
-            tree.a = gauss(-1, 1)
-            tree.b = gauss(1, 1)
-            tree.label = False
-        tree.Fill()
-    tree.write()
+# Compute and print pvalue
+# Reference: http://en.wikipedia.org/wiki/Normal_distribution
+chi = (args.zvalue - args.mean) / args.sigma
+pvalue = 0.5 * (1 - math.erf(chi / math.sqrt(2)))
+print('p-value: %s' % pvalue)
